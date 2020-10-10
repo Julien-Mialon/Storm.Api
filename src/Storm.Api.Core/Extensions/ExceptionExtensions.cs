1fe2c5164b754d77836e5764e5e9c89b39099619
Julien Mialon <mialon.julien@gmail.com>
2020-10-10 10:48:51 +0200

    add some extensions and base class to simplify usage

---
+++ src/Storm.Api.Core/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,25 @@
+using System.Net;
+using Storm.Api.Core.Exceptions;
+
+namespace Storm.Api.Core.Extensions
+{
+	public static class ExceptionExtensions
+	{
+		public static void DomainHttpCodeExceptionIfNull(this object o, HttpStatusCode statusCode, string errorCode = null, string errorMessage = null)
+		{
+			if (o is null)
+			{
+				throw new DomainHttpCodeException(statusCode, errorCode, errorMessage);
+			}
+		}
+
+		public static void UnauthorizedIfNull(this object o, string errorCode = null, string errorMessage = null)
+			=> o.DomainHttpCodeExceptionIfNull(HttpStatusCode.Unauthorized, errorCode, errorMessage);
+
+		public static void ForbiddenIfNull(this object o, string errorCode = null, string errorMessage = null)
+			=> o.DomainHttpCodeExceptionIfNull(HttpStatusCode.Forbidden, errorCode, errorMessage);
+
+		public static void NotFoundIfNull(this object o, string errorCode = null, string errorMessage = null)
+			=> o.DomainHttpCodeExceptionIfNull(HttpStatusCode.Forbidden, errorCode, errorMessage);
+	}
+}
