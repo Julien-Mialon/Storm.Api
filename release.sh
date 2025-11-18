d39489c79d27425e28a9ce72b241c78923e8887d
Julien Mialon <mialon.julien@gmail.com>
2025-11-18 19:46:43 +0100

    Release net10

---
+++ release.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+version="10.0.0"
+
+rm -rf build artifacts
+dotnet build -c Release /property:Version=$version
+
+apiKey=$NUGET_API_KEY
+source="https://api.nuget.org/v3/index.json"
+
+nuget push artifacts/Storm.Api.$version.nupkg -Source $source -ApiKey $apiKey
+nuget push artifacts/Storm.Api.Dtos.$version.nupkg -Source $source -ApiKey $apiKey
+nuget push artifacts/Storm.Api.SourceGenerators.$version.nupkg -Source $source -ApiKey $apiKey
