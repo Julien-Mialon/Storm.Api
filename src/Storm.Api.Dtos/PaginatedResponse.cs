d7b32774eb6d461041e915501c6903c1ce0ae16a
Julien Mialon <mialon.julien@gmail.com>
2025-12-02 10:18:47 +0100

    Handle mix between Newtonsoft.Json & System.Text.Json

--- src/Storm.Api.Dtos/PaginatedResponse.cs
+++ src/Storm.Api.Dtos/PaginatedResponse.cs
@@ -1,18 +1,23 @@
 using System.Text.Json.Serialization;
+using Newtonsoft.Json;
 
 namespace Storm.Api.Dtos;
 
 public class PaginatedResponse<T> : Response
 {
 	[JsonPropertyName("data")]
+	[JsonProperty("data")]
 	public T[]? Data { get; set; }
 
 	[JsonPropertyName("page")]
+	[JsonProperty("page")]
 	public int Page { get; set; }
 
 	[JsonPropertyName("count")]
+	[JsonProperty("count")]
 	public int Count { get; set; }
 
 	[JsonPropertyName("total_count")]
+	[JsonProperty("total_count")]
 	public int TotalCount { get; set; }
 }
--- src/Storm.Api.Dtos/Response.cs
+++ src/Storm.Api.Dtos/Response.cs
@@ -1,21 +1,26 @@
 ﻿using System.Text.Json.Serialization;
+using Newtonsoft.Json;
 
 namespace Storm.Api.Dtos;
 
 public class Response
 {
 	[JsonPropertyName("is_success")]
+	[JsonProperty("is_success")]
 	public bool IsSuccess { get; set; }
 
 	[JsonPropertyName("error_code")]
+	[JsonProperty("error_code")]
 	public string? ErrorCode { get; set; }
 
 	[JsonPropertyName("error_message")]
+	[JsonProperty("error_message")]
 	public string? ErrorMessage { get; set; }
 }
 
 public class Response<T> : Response
 {
 	[JsonPropertyName("data")]
+	[JsonProperty("data")]
 	public T? Data { get; set; }
 }
