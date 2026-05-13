f2a7fcae7cc804f25a994fcd87ce8f1b93fed347
Julien Mialon <mialon.julien@gmail.com>
2026-05-13 12:27:59 +0200

    Add extension to generate default value when using Type

---
+++ src/Storm.Api/Extensions/DefaultValuesExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace Storm.Api.Extensions;
+
+public static class DefaultValuesExtensions
+{
+	private static readonly Dictionary<Type, object> DEFAULT_VALUES = new();
+
+	extension(Type type)
+	{
+		public object? GetDefaultValue()
+		{
+			if (type.IsValueType is false)
+			{
+				return null;
+			}
+
+			if (DEFAULT_VALUES.TryGetValue(type, out object? value))
+			{
+				return value;
+			}
+
+			value = Activator.CreateInstance(type);
+			DEFAULT_VALUES.TryAdd(type, value!);
+			return value;
+		}
+	}
+}
